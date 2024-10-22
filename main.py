7dd69743d35ca845ee61b9576da78a35047b9e81
Hemavathi-Code20 <hemavathisiva05@gmail.com>
2024-10-22 19:59:00 +0530

    updated qrcode generator website

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+import qrcode
+from io import BytesIO
+from base64 import b64encode
+import os  
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def generateQR():
+    memory = BytesIO()
+    data = request.form.get('link')
+
+    if data:
+        image = qrcode.make(data)
+        image.save(memory, format='PNG')
+        memory.seek(0)
+        base64_image = "data:image/png;base64," + b64encode(memory.getvalue()).decode('ascii')
+    else:
+        base64_image = None
+
+    return render_template('index.html', data=base64_image)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Get the port from the environment or use 5000 by default
+    app.run(host='0.0.0.0', port=port)        # Bind to 0.0.0.0 to make the app externally accessible
